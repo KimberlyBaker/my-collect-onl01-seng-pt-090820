6246152f68ccc0faeb6c742cbe505c9bad273710
Kimberly Baker <bakerk2010@gmail.com>
2020-09-20 23:52:49 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,12 @@
-def my_collect(array)
+def my_collect(languages)
   i = 0
   name_collection = []
-  while i < array.length
-    name_collection.push yield(array[i])
-  # you could also do it this way:
-  # name_collection << yield(array[i])
+  while i < languages.length
+    name_collection << yield(array[i])
     i += 1
   end
   name_collection
 end
 
-my_collect(list) {|i| i.split(" ").first}
+my_collect(languages) {|i| i.split(" ").first}
 
