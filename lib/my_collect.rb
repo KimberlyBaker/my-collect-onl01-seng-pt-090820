3c9b0bd09b9d39d8d2c22774df3b1c823efbe7f9
Kimberly Baker <bakerk2010@gmail.com>
2020-09-20 23:56:10 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,12 +3,12 @@ students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
 
 def my_collect(languages)
   i = 0
-  name_collection = []
+  lang_collection = []
   while i < languages.length
-    name_collection << yield(array[i])
+    lang_collection << yield(array[i])
     i += 1
   end
-  name_collection
+  lang_collection
 end
 
 def my_collect(students)
